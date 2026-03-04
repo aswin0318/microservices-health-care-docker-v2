5b5c62778547c12ba11e76e7049e9fdbc5add479
Aswin S <aswinsatheesh03@gmail.com>
2026-03-05 02:16:55 +0530

    initial commit

---
+++ appointment-service/src/core/use-cases/createAppointment.js
@@ -0,0 +1,45 @@
+function createAppointmentFactory ({ appointmentRepository, logger }) {
+  return async function execute ({ request }, callback) {
+    try {
+      const { payload } = request
+
+      const {
+        companyId,
+        doctorId,
+        doctorData,
+        userId,
+        userData,
+        startTime,
+        appointmentTime,
+      } = JSON.parse(payload)
+
+      const createdAppointment = await appointmentRepository.create({
+        params: {
+          company_id: companyId,
+          doctor_id: doctorId,
+          doctor_data: doctorData,
+          user_id: userId,
+          user_data: userData,
+          start_time: startTime,
+          appointment_time: appointmentTime,
+        },
+      })
+      console.log('🚀 ~ file: createAppointment.js:27 ~ execute ~ createdAppointment:', createdAppointment)
+
+      logger.info({
+        message: 'Appointment created',
+        doctor_id: createdAppointment.doctor_id,
+        appointment_id: createdAppointment.id,
+      })
+
+      return callback(null, {
+        id: createdAppointment.id,
+        payload: JSON.stringify(createdAppointment),
+      })
+    } catch (error) {
+      throw new Error('todo')
+    }
+  }
+}
+
+export default createAppointmentFactory
